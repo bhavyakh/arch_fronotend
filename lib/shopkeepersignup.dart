0529a27ce9ed5474f106d3fe0443849cd64998b2
bhavyakh <khandelwalbhavya7@gmail.com>
2020-05-03 23:56:45 +0530

    first

---
+++ lib/shopkeepersignup.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'FadeAnimation.dart';
+
+class user extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<user> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pincodeController = TextEditingController();
+  TextEditingController storenameController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 200,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                    child: FadeAnimation(
+                  1,
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("images/1.png"),
+                      ),
+                    ),
+                  ),
+                ))
+              ],
+            ),
+          ),
+          Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Get Started!',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 50),
+              )),
+          Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Fill your details',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              )),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(30.0)),
+                border: OutlineInputBorder(),
+                labelText: 'Name',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: storenameController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(30.0)),
+                border: OutlineInputBorder(),
+                labelText: 'Store Name',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: addressController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(30.0)),
+                border: OutlineInputBorder(),
+                labelText: 'Store Address',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(30.0)),
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+            child: TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(30.0)),
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.white),
+                  suffixStyle: TextStyle(color: Colors.white)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: pincodeController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(30.0)),
+                border: OutlineInputBorder(),
+                labelText: 'Pincode',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Material(
+              elevation: 5.0,
+              color: Color(0xFFEB1555),
+              borderRadius: BorderRadius.circular(30.0),
+              child: MaterialButton(
+                onPressed: () {},
+                minWidth: 200.0,
+                height: 42.0,
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
